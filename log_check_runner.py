1a4d6f1e50db2b899da1c835a2dacb8c3a379a0e
P-Roq <pasrl_1@tutanota.com>
2024-05-06 14:50:57 +0100

    - Modified log_check_runner so that it creates an empty scrapy.log file if it does not exist.

--- log_check_runner.py
+++ log_check_runner.py
@@ -1,7 +1,12 @@
+import os
 import re
 from spider_runner import project_root_path
 
 log_path = project_root_path + '/oryx_scrape/log_files/scrapy.log'
+
+if not os.path.exists(log_path):
+    with open(log_path, 'w') as f:
+        pass
 
 with open(log_path, 'r') as file:
     log = file.readlines() 
