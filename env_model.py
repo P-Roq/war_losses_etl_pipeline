f93f45544c6b9c83b30111b74ad4ea5fd53cb340
P-Roq <pasrl_1@tutanota.com>
2024-07-05 15:45:36 +0100

    - Modified `env_model.py` to be able to connect to the database in a docker context.

--- env_model.py
+++ env_model.py
@@ -2,19 +2,36 @@ from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 import os
 
-config_path = os.getenv("PROJECTS_CONFIG")
 
-dotenv_path = f'{config_path}/oryx_war_losses_project_config/.env'
+if os.getenv("PROJECTS_CONFIG"):
 
-load_dotenv(dotenv_path)
+    config_path = os.getenv("PROJECTS_CONFIG")
+    dotenv_path = f'{config_path}/war_losses_etl_pipeline_config/.env'
 
-class Settings(BaseSettings):
-    # Database related.
-    username: str = os.getenv("USER_NAME")
-    key_db: str = os.getenv("SECRET_KEY")
-    port: str = os.getenv("PORT")
-    host: str = os.getenv("HOST")
-    database: str = os.getenv("DATABASE")
+    load_dotenv(dotenv_path)
+
+    class Settings(BaseSettings):
+        # Database related.
+        username: str = os.getenv("USER_NAME")
+        key_db: str = os.getenv("SECRET_KEY")
+        port: str = os.getenv("PORT")
+        host: str = os.getenv("HOST")
+        database: str = os.getenv("DATABASE")
+
+else:
+    class Settings(BaseSettings):
+
+        load_dotenv()
+
+        def get_secret(secret: str) -> str:
+            with open(secret, 'r') as f:
+                return f.read()
+            
+        key_db: str = get_secret(os.getenv('MYSQL_PASSWORD'))
+        database: str = os.getenv('MYQL_DATABASE')
+        username: str = os.getenv('MYSQL_USER')
+        host: str = os.getenv('MYSQL_HOST')
+        port: str = os.getenv('PORT')
 
 settings = Settings()
 
